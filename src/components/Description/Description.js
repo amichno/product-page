e2c118c42f0e37278b1b6b28d76bd01da07a3bac
amichno <95045327+amichno@users.noreply.github.com>
2022-03-24 21:05:28 +0100

    push -A

--- src/components/Description/Description.js
+++ src/components/Description/Description.js
@@ -14,16 +14,15 @@ class Description extends React.Component{
         return(
             <div className='Description'>
                <div> 
-                    <h1 className='Company'>Sneaker Company</h1>
+                    <h1 className='Company'>{this.props.Product[0].company}</h1>
 
-                    <p className='Title'>Fall Limited Edition Sneakers</p>
+                    <p className='Title'>{this.props.Product[0].title}</p>
 
-                    <p className='Text'>These low-profile sneakers are your perfect casual wear companion. Featuring a 
-                    durable rubber outer sole, they’ll withstand everything the weather can offer.</p>
+                    <p className='Text'>{this.props.Product[0].text}</p>
                 </div>
                 <div className='Prices'>
-                    <p className='AfterDiscount'>$125.00     <span className='Discount'>50%</span></p>
-                    <p className='BeforeDiscount'>$250.00</p>
+                    <p className='AfterDiscount'>$ {(this.props.Product[0].price*0.5).toFixed(2)}     <span className='Discount'>50%</span></p>
+                    <p className='BeforeDiscount'>$ {this.props.Product[0].price}</p>
                 </div>              
                 <ButtonAdd />
             </div>
